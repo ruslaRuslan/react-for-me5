11cf3ecae9b4a63a154b5388f46a551cb0eefbcd
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-03 15:32:46 +0400

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import Counter from "./components"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-
+<Counter/>
     </>
   )
 }
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,33 @@
-import reactLogo from './assets/react.svg'
+import axios from 'axios'
+import { useEffect, useState } from "react"
+import "./index.css"
 
+const url = 'https://jsonplaceholder.typicode.com/users'
+let componentYrandimi = false
 function Counter() {
-    const [counter, setCounter] = useState(0) //hook use*
+    const [users, setUsers] = useState([]) //hook use*
+    const [isDark, setIsDark] = useState(false) //hook use*
+
+    // []-> dependency list bos olanda mount-dur
+    useEffect(() => {
+        console.log('men geldim5');
+    }, [users])
+
+
+    
+    if (componentYrandimi === false) {
+        axios.get(url).then(({ data }) => {
+            setUsers(data)
+            componentYrandimi = true
+            let fromStronge = localStorage.getItem('isDark')
+            setIsDark(fromStronge === "true" ? true : false)
+        })
+    }
     return (
         <>
 
         </>
     )
 }
+
+export default Counter;
